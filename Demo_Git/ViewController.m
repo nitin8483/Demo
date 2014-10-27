cb2e2af352671ec4603f1446812c74889311c90e
Nitn <nitin8483@gmail.com>
2014-10-27 14:56:03 +0530

    iOS: Added viewDidAppear method
    added viewDidAppear method

--- Demo_Git/ViewController.m
+++ Demo_Git/ViewController.m
@@ -21,6 +21,10 @@
   // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)viewDidAppear:(BOOL)animated {
+  [super viewDidAppear:animated];
+}
+
 - (void)didReceiveMemoryWarning {
   [super didReceiveMemoryWarning];
   // Dispose of any resources that can be recreated.
